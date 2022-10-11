6416960c3f962f56a5258a6ad657dbba7270b1f3
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-11 13:11:16 +0400

    List2 added

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,9 +16,19 @@ public class Main {
         JList<String> list=new JList<>(l);
         list.setBounds(100,100,75,75);
 
+        DefaultListModel<String>l2=new DefaultListModel<>();
+        l2.addElement("Swing");
+        l2.addElement("Laravel");
+        l2.addElement("VueJS");
+        l2.addElement("Bootstrap");
+
+        JList<String> list2=new JList<>(l2);
+        list2.setBounds(100,200,75,85);
+
         frame.setSize(400,400);
         frame.setVisible(true);
         frame.setLayout(null);
         frame.add(list);
+        frame.add(list2);
     }
 }
